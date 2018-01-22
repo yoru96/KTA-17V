b61036c2ba385329d9b4a901c69543c3d7bf999d
Tarmo Teder <Tarmo.Teder@tptlive.ee>
2018-01-22 18:24:28 +0200

    22.01.2018 materjalid

--- test.php
+++ test.php
@@ -27,17 +27,31 @@ $myarr = array ('Esmaspäev','Teisipäev','Kolmapäev','Neljapäev','Reede');
 
 $number2 = $number;
 $number3 = &$number;
-
+$text = '10';
 $number = 10;
 
 $sum = $number + $number1;
 $sum2 = $number2 + $number1;
 $sum3 = $number3 + $number1;
-$sum4 = $name + $number ;
+$sum4 = $text + $number ;
 
 echo "First sum is: ".$sum." and second sum is: ".$sum2." and third sum is ".$sum3."<br>";
 echo "Text + number $sum4 <br>";
     
 echo "Minu nimi $name";
+
+echo '<br>'.$_SERVER['REMOTE_ADDR'];
+
+function log_ip(){
+    date_default_timezone_set('Europe/Tallinn');
+    $file = fopen("log.txt", "a") or die("Can't open file");
+    $time = time();
+    $txt = $_SERVER['REMOTE_ADDR']." ".date("d-m-Y H:i:s")."\n";
+    fwrite($file, $txt);
+    fclose($file);
     
+}
+
+log_ip();
+
 ?>
